e4da185454878e0973812a51525e48af4c87597a
ishraqfatin <ishraqfatin2510@gmail.com>
2023-07-25 16:18:57 +0600

    changes in server.js

--- shuchimita-app/server/server.js
+++ shuchimita-app/server/server.js
@@ -254,8 +254,8 @@ const processScan = async (scannedId, scanTime) => {
 };
 
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-	console.log(`Server running on port http://localhost:${port}`);
-});
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+// 	console.log(`Server running on port http://localhost:${port}`);
+// });
 
