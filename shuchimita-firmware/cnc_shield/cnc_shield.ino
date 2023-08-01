27263682b900ebd3aff1eb59d56e713bf9a266e7
Shihab <shihab.hasan3322@gmail.com>
2023-08-02 00:56:08 +0600

    feat: add code of the stepper motor, CNC Shield

---
+++ shuchimita-firmware/cnc_shield/cnc_shield.ino
@@ -0,0 +1,47 @@
+// defines pins numbers
+
+const int stepX = 2;
+const int dirX = 5;
+
+const int enPin = 8;
+
+const int irPin = 52;
+
+bool dispenserDetected = false;
+char instruction = "";
+
+void setup() {
+  // Sets the two pins as Outputs
+
+  pinMode(stepX, OUTPUT);
+  pinMode(dirX, OUTPUT);
+
+  pinMode(enPin, OUTPUT);
+
+  pinMode(irPin, INPUT);
+
+  digitalWrite(enPin, LOW);
+  digitalWrite(dirX, HIGH);
+
+  Serial.begin(115200);
+}
+
+void loop() {
+  // Enables the motor to move in a particular direction
+  // Makes 200 pulses for making one full cycle rotation
+  if (Serial.available()) {
+    instruction = Serial.read();
+  }
+
+
+  int val = digitalRead(irPin);
+  if (!val) dispenserDetected = true;
+  if (instruction == 's') dispenserDetected = false;
+
+  if (!dispenserDetected) {
+    digitalWrite(stepX, HIGH);
+    delayMicroseconds(50);
+    digitalWrite(stepX, LOW);
+    delayMicroseconds(50);
+  }
+}
